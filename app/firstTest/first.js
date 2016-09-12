f0992b4aeb5d7ecfd01af881b88dd97236f2b72e
QxQstar <1395294694@qq.com>
2016-09-12 16:22:54 +0800

    第一个react练习

---
+++ app/firstTest/first.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/9/12.
+ */
+var Content = React.createClass({
+    render:function(){
+        return <p>hello react </p>
+    }
+});
+ReactDOM.render(<Content/>,document.getElementById('content'));
