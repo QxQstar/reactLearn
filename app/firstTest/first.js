68a9d477742dd5be48b4e1eb2790ef7dc1fec849
QxQstar <1395294694@qq.com>
2016-09-12 17:12:32 +0800

    充实了第一个react练习

--- app/firstTest/first.js
+++ app/firstTest/first.js
@@ -1,9 +1,40 @@
 /**
  * Created by Administrator on 2016/9/12.
  */
-var Content = React.createClass({
+//    例子一
+var Content1 = React.createClass({
     render:function(){
         return <p>hello react </p>
     }
 });
-ReactDOM.render(<Content/>,document.getElementById('content'));
+var Content2 = React.createClass({
+    render:function(){
+        return (
+            React.createElement('div',{"className":"twice"},"hello react twice")
+            );
+    }
+});
+ReactDOM.render(<div>
+                    <Content1/>
+                    <Content2/>
+                </div>,
+                document.getElementById('content')
+               );
+
+
+
+
+    //例子二
+var Content3 = React.createClass({
+    render:function(){
+        return (
+            //不能使用class，因为这里不是真正的html便签
+            <div className='other'>
+                <h1>Another way</h1>
+                <Content1/>
+                <Content2/>
+            </div>
+            );
+    }
+});
+ReactDOM.render(<Content3/>,document.getElementById('contentOther'));
