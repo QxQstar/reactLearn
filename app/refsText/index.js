67df0fc2db88b37422935e64c7de08202f76f526
QxQstar <1395294694@qq.com>
2016-09-19 18:08:38 +0800

    refs 学习

---
+++ app/refsText/index.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Administrator on 2016/9/19.
+ */
+//例子一
+var TextInput = React.createClass({
+    render:function(){
+        return <input type='text' className='text' ref='input'/>;
+    },
+    componentDidMount:function(){
+        console.log(this.refs.input);
+    },
+    check:function(){
+        console.log('我是组件中的方法');
+    }
+});
+ReactDOM.render(<TextInput/>,document.getElementById('content1'));
+//例子二
+var DivBox = React.createClass({
+    render:function(){
+        return <TextInput ref='TextInputClass'/>;
+    },
+    componentDidMount:function(){
+        this.refs.TextInputClass.check();
+    }
+});
+ReactDOM.render(<DivBox/>,document.getElementById('content2'));
+
+//例子三
+var Div = React.createClass({
+    render:function(){
+        return <div ref={function(curDivNode){console.log(curDivNode);}}>
+                    kfjdk
+                </div>
+    },
+    check:function(){
+        console.log('我是组件方法')
+    }
+});
+ReactDOM.render(<Div/>,document.getElementById('content3'));
+//例子四
+var OutDiv = React.createClass({
+    render:function(){
+        return <Div ref={function(outDivNode){
+            outDivNode.check();
+//            console.log(outDivNode)
+        }}/>
+    }
+
+});
+ReactDOM.render(<OutDiv/>,document.getElementById('content4'));
